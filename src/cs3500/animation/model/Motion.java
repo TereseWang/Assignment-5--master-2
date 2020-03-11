7743d1a96d3fd3ceba51cdd7f3b28991d794916e
ezrima <57233994+ezrima@users.noreply.github.com>
2020-03-11 17:53:03 -0400

    1

--- src/cs3500/animation/model/Motion.java
+++ src/cs3500/animation/model/Motion.java
@@ -28,6 +28,75 @@ public class Motion {
     this.endShape = enshape;
   }
 
+
+  //modify
+
+  /**
+   * push the timeline forward with the length of given period.
+   *
+   * @param period the given length
+   * @throws IllegalArgumentException if it can't be push forward at that long.
+   */
+  public void pushForward(int period) {
+    if (startTick - period < 0) {
+      throw new IllegalArgumentException("can't push thie motion forward");
+    }
+    startTick = startTick - period;
+    endTick = endTick - period;
+
+  }
+
+  /**
+   * push the timeline backward with the length of given period.
+   *
+   * @param period the given length
+   */
+  public void pushBackward(int period) {
+    startTick = startTick + period;
+    endTick = endTick + period;
+  }
+
+  /**
+   * extend the timeline to the given point.
+   *
+   * @param endpoint the given point
+   */
+  public void changeTimeline(int endpoint) {
+    endTick = endpoint;
+  }
+
+  /**
+   * change the color of the shape within this timeLine
+   *
+   * @param color the desired color
+   */
+  public void changeColor(Color color) {
+    startShape.changeColor(color);
+  }
+
+  /**
+   * change the position of the final shape.
+   *
+   * @param position the desired position
+   */
+  public void changePosition(Posn position) {
+    endShape.changePosn(position);
+  }
+
+  /**
+   * change the size of the final shape.
+   *
+   * @param width  the width of the shape
+   * @param height the height of the shape
+   * @throws IllegalArgumentException if the given width and height are invalid.
+   */
+  public void changeSize(int width, int height) {
+    endShape.changeSize(width, height);
+  }
+
+  //get info
+
+
   /**
    * get the shape after motion.
    *
@@ -38,73 +107,31 @@ public class Motion {
   }
 
   /**
+   * get the starting point of the timeline.
+   *
+   * @return int as the starting point
+   */
+  public int getStartPoint() {
+    return startTick;
+  }
+
+  /**
+   * determine whether the given motion is adjacent next to this motion.
+   *
+   * @param other given motion
+   * @return boolean as the result
+   */
+  public boolean isConnect(Motion other) {
+    return endTick == other.getStartPoint() && endShape.equals(other.getFinalImages());
+  }
+
+  /**
    * compute the length of the timeline of this motion.
    *
    * @return int as the length of timeline
    */
   int getPeriod() {
     return endTick - startTick;
-  }
-
-  //modify
-
-  /**
-   * push the timeline forward with the length of given period.
-   * @param period the given length
-   * @throws IllegalArgumentException if it can't be push forward at that long.
-   */
-  public void pushForward(int period){
-    if(startTick-period<0){
-      throw new IllegalArgumentException("can't push thie motion forward");
-    }
-    startTick = startTick - period;
-    endTick = endTick -period;
-
-  }
-
-  /**
-   * push the timeline backward with the length of given period.
-   * @param period the given length
-   */
-  public void pushBackward(int period){
-    startTick = startTick + period;
-    endTick = endTick + period;
-  }
-
-  /**
-   * extend the timeline to the given point.
-   * @param endpoint the given point
-   */
-  public void changeTimeline(int endpoint){
-    endTick = endpoint;
-  }
-
-  /**
-   * change the color of the shape within this timeLine
-   * @param color the desired color
-   */
-  public void changeColor(Color color){
-    startShape.changeColor(color);
-    endShape.changeColor(color);
-
-  }
-
-  /**
-   * change the position of the final shape.
-   * @param position the desired position
-   */
-  public void changePosition(Posn position){
-    endShape.changePosn(position);
-  }
-
-  /**
-   * change the size of the final shape.
-   * @param width the width of the shape
-   * @param height the height of the shape
-   * @throws IllegalArgumentException if the given width and height are invalid.
-   */
-  public void changeSize(int width, int height){
-    endShape.changeSize(width,height);
   }
 
 }
--- src/cs3500/animation/model/SimpleAnimation.java
+++ src/cs3500/animation/model/SimpleAnimation.java
@@ -1,19 +1,21 @@
 package cs3500.animation.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Set;
 
 /**
  * a simple animation class, use list of motion as representation of sequence of motions.
  */
-public class SimpleAnimation implements Animation<List<Motion>>{
+public class SimpleAnimation implements Animation<List<Motion>> {
   LinkedHashMap<String, List<Motion>> animation;
 
   /**
    * construct an empty animation.
    */
-  public SimpleAnimation(){
+  public SimpleAnimation() {
     animation = new LinkedHashMap<>();
   }
 
@@ -24,14 +26,32 @@ public class SimpleAnimation implements Animation<List<Motion>>{
 
   @Override
   public void addMotion(String name, Motion motion) {
-
+    validate(name);
+    List<Motion> sequence =  animation.get(name);
+    Motion last = sequence.get(sequence.size() - 1);
+    if(last.get)
   }
 
   /**
-   * check whether there is a shape is
+   * check whether there is a shape's name as the given string.
+   *
    * @param name
+   * @throws IllegalArgumentException if couldn't find it.
    */
-  private void validate(String name)
+  private void validate(String name) {
+    Set<String> nameSet = animation.keySet();
+    Iterator nameI = nameSet.iterator();
+    boolean find = false;
+    while (nameI.hasNext()) {
+      if (nameI.next().equals(name)) {
+        find = true;
+      }
+    }
+    if (!find) {
+      throw new IllegalArgumentException("couldn't find the shape: " + name);
+    }
+  }
+
 
   @Override
   public void deleteShape(String name) {
