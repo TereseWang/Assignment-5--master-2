6ff1eef7963f17f452e06867f3f6fc7e395dc19a
TereseWang <wang.haoqi@husky.neu.edu>
2020-03-11 16:01:35 -0400

    ...

--- test/ColorTest.java
+++ test/ColorTest.java
@@ -4,7 +4,17 @@ import org.junit.Test;
 public class ColorTest {
 
   @Test(expected = IllegalArgumentException.class)
-  public void testInvalidColor() {
+  public void testInvalidColorR() {
     Color c = new Color(-1, 100, 200);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidColorR1() {
+    Color c = new Color(300, 100, 200);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidColorG() {
+    Color c = new Color(200, -100, 200);
   }
 }
