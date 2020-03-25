846465f6dd2030405cb6eb56d0e0c82a262c6216
ezrima <57233994+ezrima@users.noreply.github.com>
2020-03-25 06:11:50 -0400

    1

---
+++ src/cs3500/animator/view/View.java
@@ -0,0 +1,13 @@
+package cs3500.animator.view;
+
+public interface View {
+  /**
+   * Refresh the view to reflect any changes in the game state.
+   */
+  void refresh();
+
+  /**
+   * Make the view visible to start the game session.
+   */
+  void makeVisible();
+}
