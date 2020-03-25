366ad0d3a6bb5e3abdd8b0178f0d2b509d21faf9
ezrima <57233994+ezrima@users.noreply.github.com>
2020-03-25 19:39:07 -0400

    1

--- src/cs3500/animator/view/TextualView.java
+++ src/cs3500/animator/view/TextualView.java
@@ -1,4 +1,24 @@
 package cs3500.animator.view;
 
-public class TextualView {
+import org.w3c.dom.Text;
+
+import cs3500.animation.model.Animation;
+
+public class TextualView implements View{
+  private  final Animation model;
+
+
+  public TextualView(Animation model){
+    this.model = model;
+  }
+  @Override
+  public void refresh() {
+    throw  new UnsupportedOperationException("Textual view can't be refresh.");
+
+  }
+
+  @Override
+  public void display() {
+System.out.print();
+  }
 }
--- src/cs3500/animator/view/View.java
+++ src/cs3500/animator/view/View.java
@@ -3,11 +3,12 @@ package cs3500.animator.view;
 public interface View {
   /**
    * Refresh the view to reflect any changes in the game state.
+   * @throws  UnsupportedOperationException if the view didn't support this operation
    */
   void refresh();
 
   /**
    * Make the view visible to start the game session.
    */
-  void makeVisible();
+  void display();
 }
