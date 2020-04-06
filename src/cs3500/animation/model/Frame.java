6f4f47f5a1a85e87d9f68fae7c8dc745fd113efe
ezrima <57233994+ezrima@users.noreply.github.com>
2020-04-06 13:56:30 -0400

    1

--- src/cs3500/animation/model/Frame.java
+++ src/cs3500/animation/model/Frame.java
@@ -2,14 +2,21 @@ package cs3500.animation.model;
 
 import cs3500.animator.shape.Shape;
 
+import static java.lang.String.format;
+
 /**
  * represent a keyFrame. It cantains a state and the time of the state.
  */
-public class KeyFrame {
+public class Frame {
   private Shape state;
   private int time;
 
-  public KeyFrame(Shape state, int time){
+  /**
+   * construct a
+   * @param state
+   * @param time
+   */
+  public Frame(Shape state, int time){
     if(state == null || time < 0){
       throw  new IllegalArgumentException("the time cane't be less than zero and state can't be " +
               "null");
@@ -18,9 +25,19 @@ public class KeyFrame {
     this.time = time;
   }
 
+
   @Override
   public boolean equals(Object o){
-    if (o instanceof KeyFrame);
-  return true;}
+    if (o instanceof Frame){
+      return ((Frame) o).state == state && ((Frame) o).time == time;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public String toString() {
+    return time +" " + state.toString();
+  }
 
 }
